c1e63e527bfecabff0a2b038285fea36e1ec6630
Mirza Masic <masic.mirza@gmail.com>
2021-09-23 13:13:38 +0200

    First commit, adding ex1-4

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+// Create a monitorsListArray array here
+
+// End of monitorsListArray array
+
+function myMonitorsFunction(arr) {
+    var newMonitorsFunctionList = [...arr];
+    // Only change code below this line
+
+    // Only change code above this line
+}
+
+console.log(myMonitorsFunction()); // Change this line
+module.exports = myMonitorsFunction;
